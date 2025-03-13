de679183151d277f3d6e3e4c53c7af5eef19a181
Phuoc Sanh <sanh.tp@mekongsoft.com.vn>
2025-03-13 14:04:01 +0700

    log

--- server.js
+++ server.js
@@ -14,9 +14,20 @@ app.use(
     target: process.env.HOST, // API backend của bạn
     changeOrigin: true,
     pathRewrite: { "^/api": "" },
+    logLevel: "debug", // Log chi tiết từ http-proxy-middleware
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(`Proxying request: ${req.method} ${req.url}`)
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      console.log(
+        `Received response: ${req.method} ${req.url} with status ${proxyRes.statusCode}`
+      )
+    },
+    onError: (err, req, res) => {
+      console.error("Proxy error:", err)
+    },
   })
 )
-
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Proxy server đang chạy tại cổng ${PORT}`)
